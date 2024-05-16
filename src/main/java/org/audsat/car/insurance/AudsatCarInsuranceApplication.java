93d156b0f5f1ab914d597c9035629083d641b500
williamsahumada <wiljosbocanegra@rd.com.br>
2024-05-16 00:01:21 -0300

    Commit inicial

---
+++ src/main/java/org/audsat/car/insurance/AudsatCarInsuranceApplication.java
@@ -0,0 +1,11 @@
+package org.audsat.car.insurance;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AudsatCarInsuranceApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(AudsatCarInsuranceApplication.class, args);
+    }
+}
